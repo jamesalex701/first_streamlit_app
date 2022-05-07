919f34fc958f3b192c953b9d9d22ebd88f5df07b
jamesalex701 <83220687+jamesalex701@users.noreply.github.com>
2022-05-07 15:21:09 -0300

    Update streamlit_app.py
    Added breakfast favourites

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My New Streamlit App')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
