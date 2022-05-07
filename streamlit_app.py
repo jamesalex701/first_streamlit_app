9102118671d9d37ab46ad413e44369973d21b8fc
jamesalex701 <83220687+jamesalex701@users.noreply.github.com>
2022-05-07 15:39:34 -0300

    Update streamlit_app.py
    added new code to enable the app to show only the rows rom dataset which are selected from the multiselect list

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,6 @@ import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list=my_fruit_list.set_index('Fruit')
-streamlit.multiselect("pick some fruits: ",list(my_fruit_list.index))
-
+fruits_select=streamlit.multiselect("pick some fruits: ",list(my_fruit_list.index))
+fruits_show=my_fruit_list.loc[fruits_select]
 streamlit.dataframe(my_fruit_list)
